44ed28b4ba5f19d1218a0d42cc35a65560fd8b3e
Paramjeet-pmt <paramjeet.tech07@gamil.com>
2025-11-02 08:52:10 +0530

    Program to check the number is perfect number or not

---
+++ Find Number is perfect or not.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    int n ;
+
+    printf("Enter a number: ");
+    if (scanf("%d", &n) != 1)
+        return 0;
+    int sum = 0;
+    for ( int i = 1; i <= n / 2; i++){
+        if (n % i == 0)
+            sum += i;
+    }
+    if (sum == n){
+        printf("Perfect number\n");
+    }
+    else{
+        printf("Not a perfect number\n");
+    }
+    return 0;
+}
