a1ff921d6499af9e37f6f68409e704e1f2146eb0
Paramjeet-pmt <231895267+Paramjeet-pmt@users.noreply.github.com>
2025-11-25 22:56:49 +0530

    Making neccessary Changes

--- NEWPROGR.c
+++ NEWPROGR.c
@@ -1,26 +1,29 @@
 #include<stdio.h>
 
 int main(){
-	int a,b,c,temp = 0;
+	int n,i = 0,fabNm2=0,fabNm1=1,fabN=0;
 	
-	printf("Enter 3 number :");
-	scanf("%d %d %d",&a,&b,&c);
+	printf("Enter the Number of term of Fibonacci series: ");
+	scanf("%d",&n);
 	
-	if ( a == b && b == c){
-		printf("All the Number are equal");
+	printf("The Fibanonaci Series is :\n");
+	if(n >= 1){
+		fabN = 0;
+	}
+		printf("%d\n",fabN);
+	if(n >= 2){
+		fabN = 1;
+	}
+		printf("%d\n",fabN);
+	
+	for( i = 3; i<=n; i++){
+		fabN = fabNm2 + fabNm1;
+		printf("%d\n",fabN);
+		fabNm2 = fabNm1;
+		fabNm1 = fabN;
 	}
 	
-	else {
-		if( a >= b && a >= c){
-			printf("Largest number :%d",a);
-		}else {
-			if( b >= c && b >= c){
-				printf("Largest number :%d",b);
-			}else{
-				printf("Largest number :%d",c);
-			}
-		}
-	}
+	
 	
 	return 0;
 }
