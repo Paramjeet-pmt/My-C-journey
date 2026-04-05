db8d1a01144a219c46f4902d50927a085b2f93ad
Paramjeet-pmt <231895267+Paramjeet-pmt@users.noreply.github.com>
2026-04-06 00:49:31 +0530

    Program for creation dynamic array using malloc

--- Dynamic_array_using_malloc.c
+++ Dynamic_array_using_malloc.c
@@ -9,10 +9,10 @@ int main(){
 
     int*ptr = (int*)malloc(n*sizeof(int));
 
-    for(int i=0; i<=n; i++){
-        printf("Enter the %d element of the array: ",i);
-        scanf("%d",&ptr[i]);
-    }
+    // for(int i=0; i<=n; i++){
+    //     printf("Enter the %d element of the array: ",i);
+    //     scanf("%d",&ptr[i]);
+    // }
 
 
     for(int i = 0; i<=n; i++){
