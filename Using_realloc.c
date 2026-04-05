8face669ce632fffe3fefbb8905da129e037aa93
Paramjeet-pmt <231895267+Paramjeet-pmt@users.noreply.github.com>
2026-04-06 01:22:20 +0530

    Program for changing size of dynamic array using realloc

---
+++ Using_realloc.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int n;
+    printf("Enter the size of array: ");
+    scanf("%d",&n);
+
+    int* ptr = (int*)calloc(n,sizeof(int));
+
+    for(int i = 0; i<=n; i++){
+        printf("Enter the %d element of the array: ",i);
+        scanf("%d",&ptr[i]);
+    }
+
+    for(int i = 0; i<=n; i++){
+        printf("The %d element of array is %d\n",i,ptr[i]);
+    }
+
+    printf("Enter the size of realloc array: ");
+    scanf("%d",&n);
+
+     ptr = (int*)realloc(ptr,n*sizeof(int));
+
+    for(int i = 0; i<=n; i++){
+        printf("Enter the %d element of the array: ",i);
+        scanf("%d",&ptr[i]);
+    }
+
+    for(int i = 0; i<=n; i++){
+        printf("The %d element of array is %d\n",i,ptr[i]);
+    }
+
+}
